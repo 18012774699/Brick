d317679912598cdf3e437d897c92e91250b824a8
Will <2623556566@qq.com>
2020-05-22 20:12:42 +0800

    asd

--- ML/stochastic_gradient_descent.py
+++ ML/stochastic_gradient_descent.py
@@ -2,33 +2,31 @@
 import numpy as np
 
 X0 = np.ones((100, 1))
-# 随机样本因素
-X1 = 2 * np.random.rand(100, 1)
+X1 = 2 * np.random.rand(100, 1)     # 随机样本因素
 X = np.c_[X0, X1]
-y = 4 + 3 * X1 + np.random.randn(100, 1)
-print(X)
+y = 9 + 5 * X1 + np.random.randn(100, 1)
+# print(X)
 
 t0, t1 = 5, 50  # 超参数
 n_epochs = 500  # 迭代次数
-m = 10000       # 样本数
+m = 1000        # 样本数
 
 
-# 不断减小学习率
 def learning_schedule(t):
     return t0 / (t + t1)
 
 
 theta = np.random.randn(2, 1)
-# range[0,x)
+
 for epoch in range(n_epochs):
     for i in range(m):
-        # 返回[0, m)随机整数
         random_index = np.random.randint(m)
-        # 提取对应的随机样本
         xi = X[random_index:random_index + 1]
         yi = y[random_index:random_index + 1]
         gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
         learning_rate = learning_schedule(epoch * m + i)
         theta = theta - learning_rate * gradients
+    # if epoch % 50:
+    #     print(theta)
 
 print(theta)

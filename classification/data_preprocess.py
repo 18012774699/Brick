3bd081459b7a1c08fbc4a74f6355b4c14d138fc0
Will <2623556566@qq.com>
2020-05-24 10:30:35 +0800

    asd

--- classification/data_preprocess.py
+++ classification/data_preprocess.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from Api import img_io
 
 train_path = r"D:\AI\dataset\图像\dogs-cats-images\dataset\training_set"
@@ -7,18 +8,56 @@ train_path_set = img_io.read_dir(train_path)
 test_path_set = img_io.read_dir(test_path)
 print(len(train_path_set))
 
-label_train = []
-label_test = []
 
-for path in train_path_set:
-    if path.find('cats/cat') > -1:
-        label_train.append(0)
-    else:
-        label_train.append(1)
+def mark_dataset(data_path_set):
+    label_set = []
+    for path in data_path_set:
+        if path.find('cats/cat') > -1:
+            label_set.append(0)
+        else:
+            label_set.append(1)
+    return data_path_set, label_set
 
-for path in test_path_set:
-    if path.find('cats/cat') > -1:
-        label_test.append(0)
-    else:
-        label_test.append(1)
+
+train_path_set, label_train = mark_dataset(train_path_set)
+test_path_set, label_test = mark_dataset(test_path_set)
+
+
+# 将image和label转为list格式数据，因为后边用到的的一些tensorflow函数接收的是list格式数据
+# 为了方便网络的训练，输入数据进行batch处理
+# image_W, image_H, ：图像高度和宽度
+# batch_size：每个batch要放多少张图片
+# capacity：一个队列最大多少
+def get_batch(image, label, image_W, image_H, batch_size, capacity):
+    # step1：将上面生成的List传入get_batch() ，转换类型，产生一个输入队列queue
+    # tf.cast()用来做类型转换
+    image = tf.cast(image, tf.string)  # 可变长度的字节数组.每一个张量元素都是一个字节数组
+    label = tf.cast(label, tf.int32)
+    # tf.train.slice_input_producer是一个tensor生成器
+    # 作用是按照设定，每次从一个tensor列表中按顺序或者随机抽取出一个tensor放入文件名队列。
+    input_queue = tf.train.slice_input_producer([image, label])
+    label = input_queue[1]
+    image_contents = tf.read_file(input_queue[0])  # tf.read_file()从队列中读取图像
+
+    # step2：将图像解码，使用相同类型的图像
+    image = tf.image.decode_jpeg(image_contents, channels=3)
+    # jpeg或者jpg格式都用decode_jpeg函数，其他格式可以去查看官方文档
+
+    # step3：数据预处理，对图像进行旋转、缩放、裁剪、归一化等操作，让计算出的模型更健壮。
+    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
+    # 对resize后的图片进行标准化处理
+    image = tf.image.per_image_standardization(image)
+
+    # step4：生成batch
+    # image_batch: 4D tensor [batch_size, width, height, 3], dtype = tf.float32
+    # label_batch: 1D tensor [batch_size], dtype = tf.int32
+    image_batch, label_batch = tf.train.batch([image, label], batch_size=batch_size, num_threads=16, capacity=capacity)
+
+    # 重新排列label，行数为[batch_size]
+    label_batch = tf.reshape(label_batch, [batch_size])
+    # image_batch = tf.cast(image_batch, tf.uint8)    # 显示彩色图像
+    image_batch = tf.cast(image_batch, tf.float32)  # 显示灰度图
+    # print(label_batch) Tensor("Reshape:0", shape=(6,), dtype=int32)
+    return image_batch, label_batch
+    # 获取两个batch，两个batch即为传入神经网络的数据
 

8befdf7ad603f730c753413e34a046f8d2c084b4
Will <2623556566@qq.com>
2020-05-24 09:05:19 +0800

    asd

--- Api/img_io.py
+++ Api/img_io.py
@@ -1,5 +1,7 @@
 import os
+import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 def match_extension(file_name: str, file_type: list) -> bool:
@@ -8,8 +10,8 @@ def match_extension(file_name: str, file_type: list) -> bool:
     return extension in file_type
 
 
-def read_dir(search_path: str, file_type: list):
-    img_list = []
+def read_dir(search_path: str, file_type: list = [".jpg"]):
+    img_path_list = []
     for main_dir, subdir, file_name_list in os.walk(search_path):
         # print("main_dir:", main_dir)  # 当前主目录
         # print("subdir:", subdir)  # 当前主目录下的所有目录
@@ -18,6 +20,17 @@ def read_dir(search_path: str, file_type: list):
             if not match_extension(filename, file_type):
                 continue
             file_path = main_dir + '/' + filename
-            img = cv2.imread(file_path)
-            img_list.append(img)
-    return img_list
+            img_path_list.append(file_path)
+    return img_path_list
+
+
+if __name__ == '__main__':
+    test_path = r"D:\AI\dataset\图像\dogs-cats-images\dataset\test_set"
+    test_set = read_dir(test_path)
+    # image = cv2.cvtColor(test_set[0], cv2.COLOR_BGR2RGB)
+    img = plt.imread(test_set[0])
+
+    # img.astype(np.float32)    # for train
+    # img.astype(np.uint8)      # for show
+    plt.imshow(img)
+    plt.show()
--- classification/classification.py
+++ classification/classification.py
@@ -1,8 +1,8 @@
-import numpy as np
+from Api import img_io
 
 train_path = r"D:\AI\dataset\图像\dogs-cats-images\dataset\training_set"
 test_path = r"D:\AI\dataset\图像\dogs-cats-images\dataset\test_set"
 
-
-
-
+# train_set = img_io.read_dir(train_path)
+# test_set = img_io.read_dir(test_path)
+# print(len(train_set))

c207a94744ca7285a75d4c11a972cb5c2c9de216
Will <2623556566@qq.com>
2020-05-24 17:53:07 +0800

    asd

--- Semantic_segmentation/Semantic_segmentation.py
+++ Semantic_segmentation/Semantic_segmentation.py
@@ -1,5 +1,26 @@
+import os
 import tensorflow as tf
 from Api import img_load
+
+
+def match_ground_truth(filename, filter_list):
+    (file1, extension1) = os.path.splitext(filename)
+    for file in filter_list:
+        (file2, extension2) = os.path.splitext(file)
+        if file1 == file2:
+            return True
+    return False
+
+
+def read_file_by_name(search_path: str, filter_list: list):
+    img_path_list = []
+    for main_dir, subdir, file_name_list in os.walk(search_path):
+        for filename in file_name_list:
+            if not match_ground_truth(filename, filter_list):
+                continue
+            file_path = main_dir + '/' + filename
+            img_path_list.append(file_path)
+    return img_path_list
 
 
 def mark_dataset(data_path_set):
@@ -13,9 +34,10 @@ def mark_dataset(data_path_set):
 
 
 if __name__ == '__main__':
-    image_path = r"D:\AI\dataset\图像\dogs-cats-images\dataset\training_set"
+    image_path = r"D:\AI\dataset\PASCAL VOC\VOCtrainval_11-May-2012\VOCdevkit\VOC2012\JPEGImages"
     label_path = r"D:\AI\dataset\PASCAL VOC\VOCtrainval_11-May-2012\VOCdevkit\VOC2012\SegmentationClass"
     ground_truth = img_load.read_dir_by_name(label_path)
+    image = read_file_by_name(image_path, ground_truth)
     ground_truth = [label_path + '\\' + filename for filename in ground_truth]
     train_path_set = img_load.read_dir_by_name(image_path)
     print(len(train_path_set))

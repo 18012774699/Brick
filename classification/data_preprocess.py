2d45a30a333e991e9a5e43afd248319c4c29e65f
Will <2623556566@qq.com>
2020-05-24 10:50:54 +0800

    asd

--- classification/data_preprocess.py
+++ classification/data_preprocess.py
@@ -24,12 +24,9 @@ test_path_set, label_test = mark_dataset(test_path_set)
 
 
 # 将image和label转为list格式数据，因为后边用到的的一些tensorflow函数接收的是list格式数据
-# 为了方便网络的训练，输入数据进行batch处理
-# image_W, image_H, ：图像高度和宽度
 # batch_size：每个batch要放多少张图片
 # capacity：一个队列最大多少
-def get_batch(image, label, image_W, image_H, batch_size, capacity):
-    # step1：将上面生成的List传入get_batch() ，转换类型，产生一个输入队列queue
+def get_batch(image, label, image_size, batch_size, capacity):
     # tf.cast()用来做类型转换
     image = tf.cast(image, tf.string)  # 可变长度的字节数组.每一个张量元素都是一个字节数组
     label = tf.cast(label, tf.int32)
@@ -37,15 +34,15 @@ def get_batch(image, label, image_W, image_H, batch_size, capacity):
     # 作用是按照设定，每次从一个tensor列表中按顺序或者随机抽取出一个tensor放入文件名队列。
     input_queue = tf.train.slice_input_producer([image, label])
     label = input_queue[1]
-    image_contents = tf.read_file(input_queue[0])  # tf.read_file()从队列中读取图像
+    image_contents = tf.read_file(input_queue[0])  # 读取图片
 
     # step2：将图像解码，使用相同类型的图像
     image = tf.image.decode_jpeg(image_contents, channels=3)
     # jpeg或者jpg格式都用decode_jpeg函数，其他格式可以去查看官方文档
 
     # step3：数据预处理，对图像进行旋转、缩放、裁剪、归一化等操作，让计算出的模型更健壮。
-    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
-    # 对resize后的图片进行标准化处理
+    image = tf.image.resize_image_with_crop_or_pad(image, image_size, image_size)
+    # 标准化处理
     image = tf.image.per_image_standardization(image)
 
     # step4：生成batch
@@ -53,11 +50,9 @@ def get_batch(image, label, image_W, image_H, batch_size, capacity):
     # label_batch: 1D tensor [batch_size], dtype = tf.int32
     image_batch, label_batch = tf.train.batch([image, label], batch_size=batch_size, num_threads=16, capacity=capacity)
 
-    # 重新排列label，行数为[batch_size]
     label_batch = tf.reshape(label_batch, [batch_size])
     # image_batch = tf.cast(image_batch, tf.uint8)    # 显示彩色图像
     image_batch = tf.cast(image_batch, tf.float32)  # 显示灰度图
     # print(label_batch) Tensor("Reshape:0", shape=(6,), dtype=int32)
     return image_batch, label_batch
-    # 获取两个batch，两个batch即为传入神经网络的数据
 

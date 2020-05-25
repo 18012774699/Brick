f7c63491804d36817de3e81aa392aca3d517f8f1
Will <2623556566@qq.com>
2020-05-26 00:10:01 +0800

    asd

--- cnn_net/pspnet.py
+++ cnn_net/pspnet.py
@@ -2,8 +2,7 @@ from tensorflow import keras
 from cnn_net.resnet101 import get_resnet101
 
 
-def pspnet():
-    input_shape = (256, 256, 3)
+def pspnet(input_shape=(224, 224, 3)):
     image_input = keras.layers.Input(shape=input_shape)
     output_categories = 21
     layers = get_resnet101(image_input)
@@ -13,13 +12,13 @@ def pspnet():
         layer = keras.layers.Conv2D(128, 1, use_bias=False)(layer)
         layer = keras.layers.BatchNormalization()(layer)
         layer = keras.layers.Activation('relu')(layer)
-        # print(layer.shape)
+        print(layer.shape)
         if layer.shape[1] != input_shape[0]:
-            size = input_shape[0]/layer.shape[1]
+            size = input_shape[0] / layer.shape[1]
             layer = keras.layers.UpSampling2D(size=(size, size))(layer)
         UpSample.append(layer)
         # print(layer.shape)
-    concat = keras.layers.concatenate(UpSample)     # (None, 256, 256, 640)
+    concat = keras.layers.concatenate(UpSample)  # (None, 256, 256, 640)
     x = keras.layers.Conv2D(100, 1, use_bias=False)(concat)
     x = keras.layers.BatchNormalization()(x)
     x = keras.layers.Activation('relu')(x)
@@ -32,7 +31,7 @@ def pspnet():
 
 
 if __name__ == '__main__':
-    model = pspnet()
+    model = pspnet(input_shape=(224, 224, 3))
     print(model.summary())
     # model.compile(loss="mse", optimizer=keras.optimizers.SGD(lr=1e-3))
     # history = model.fit(X_train_A, y_train, epochs=20, validation_data=(X_valid_A, y_valid))
--- z_demo/Semantic_segmentation.py
+++ z_demo/Semantic_segmentation.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from Api import img_load
 from cnn_net.pspnet import pspnet
 
+TRAIN_SIZE = 224
 image_org = []
 ground_truth = []
 '''
@@ -16,7 +17,7 @@ ground_truth = []
 11:diningtable  12:dog      13:horse    14:motorbike    15:person
 16:pottedplant  17:sheep    18:sofa     19:train        20:monitor
 '''
-pixel_map_to_label = {(0, 0, 0): 0, (255, 255, 255): 0,
+pixel_map_to_label = {(0, 0, 0): 0, (224, 224, 192): 0,
                       (128, 0, 0): 1, (0, 128, 0): 2, (128, 128, 0): 3, (0, 0, 128): 4, (128, 0, 128): 5,
                       (0, 128, 128): 6, (128, 128, 128): 7, (64, 0, 0): 8, (192, 0, 0): 9, (64, 128, 0): 10,
                       (192, 128, 0): 11, (64, 0, 128): 12, (192, 0, 128): 13, (64, 128, 128): 14, (192, 128, 128): 15,
@@ -24,7 +25,7 @@ pixel_map_to_label = {(0, 0, 0): 0, (255, 255, 255): 0,
 
 with tf.name_scope('tool'):
     # 随机裁剪，数据增强
-    def rand_crop(image, label, size=(256, 256)):
+    def rand_crop(image, label, size=(TRAIN_SIZE, TRAIN_SIZE)):
         height1 = random.randint(0, image.shape[0] - size[0])
         width1 = random.randint(0, image.shape[1] - size[1])
         height2 = height1 + size[0]
@@ -32,75 +33,65 @@ with tf.name_scope('tool'):
 
         image = image[height1:height2, width1:width2]
         label = label[height1:height2, width1:width2]
-        assert image.shape == (256, 256, 3)
-        assert label.shape == (256, 256, 3)
+
+        assert image.shape == (TRAIN_SIZE, TRAIN_SIZE, 3)
+        assert label.shape == (TRAIN_SIZE, TRAIN_SIZE, 3)
         return image, label
 
-
+    # (224, 224, 3) => (224, 224, 1)
     def pixel_to_label(image):
-        plt.imshow(image)
-        plt.show()
-        img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        plt.imshow(img_gray)
-        plt.show()
+        # 用灰度图生成label
+        label = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         for i in range(image.shape[0]):
             for j in range(image.shape[1]):
-                print(image[i][j])
-                img_gray[i][j] = pixel_map_to_label[image[i][j]]
-                # if all(image[i][j] == [0, 0, 0]):
-                #     img_gray[i][j] = 0
-        print(image.shape)
+                pixel = tuple(image[i][j].tolist())
+                label[i][j] = pixel_map_to_label[pixel]
+        return label.reshape(TRAIN_SIZE, TRAIN_SIZE, 1)
 
-
-    def label_to_pixel():
-        pass
+    # (224, 224, 1) => (224, 224, 3)
+    def label_to_pixel(label):
+        label = np.repeat(label, 3, axis=2)
+        print(label.shape)
 
 with tf.name_scope('img_preprocess'):
     '''
     image_org:
-        1、加载图片
-        2、resize放大较小图片
-        3、随机裁剪（数据增强）
-        4、像素值/255，归一化
+        1、加载图片，过滤不满足尺寸的图片
+        2、随机裁剪（数据增强）
+        3、像素值/255，归一化
     ground_truth:
-        1~3、同上
-        4、像素值转分类标签（用于训练）
-        5、分类标签转像素值（用于预测结果预览）
+        1~2、同上
+        3、像素值转分类标签（用于训练）
+        4、分类标签转像素值（用于预测结果预览）
     '''
     image_org_path = r"D:\AI\dataset\PASCAL VOC\ImageQrg"
     ground_truth_path = r"D:\AI\dataset\PASCAL VOC\SegmentationClass"
     image_org_path = img_load.read_dir(image_org_path)
     ground_truth_path = img_load.read_dir(ground_truth_path)
 
-    # img.astype(np.float32)    # for train
-    # img.astype(np.uint8)      # for show
-
     # 加载图片，过滤尺寸太小的图片
     for index in range(len(image_org_path)):
         img = cv2.imread(image_org_path[index])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        if min(img.shape[0], img.shape[1]) * 2 < 256:
+        if min(img.shape[0], img.shape[1]) < TRAIN_SIZE:
             continue
         image_org.append(img)  # image_org
         img = cv2.imread(ground_truth_path[index])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         ground_truth.append(img)  # ground_truth
 
-    # 将小于目标尺寸的图片放大一倍
-    for index in range(len(ground_truth)):
-        img = image_org[index]
-        if img.shape[0] < 256 or img.shape[1] < 256:
-            img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
-            image_org[index] = img
-            ground_truth[index] = cv2.resize(ground_truth[index], (img.shape[1], img.shape[0]))
-
     # 随机裁剪，数据增强
     for index in range(len(ground_truth)):
         image_org[index], ground_truth[index] = rand_crop(image_org[index], ground_truth[index])
 
+    # img.astype(np.float32)    # for train
+    # img.astype(np.uint8)      # for show
+    # image_org = image_org / 255
+
     # ground_truth像素值转分类标签（用于训练）
-    for img in ground_truth:
-        pixel_to_label(img)
+    for index in range(len(ground_truth)):
+        ground_truth[index] = pixel_to_label(ground_truth[index])
+        label_to_pixel(ground_truth[0])
 
     # 打乱顺序
     index = [i for i in range(len(image_org))]
@@ -108,10 +99,10 @@ with tf.name_scope('img_preprocess'):
 
     image_org = np.array(image_org)[index]
     ground_truth = np.array(ground_truth)[index]
-    print(image_org.shape)  # (2912, 256, 256, 3)
-    print(ground_truth.shape)  # (2912, 256, 256, 1)
+    print(image_org.shape)  # (2892, 224, 224, 3)
+    print(ground_truth.shape)  # (2892, 224, 224, 1)
 
-# model = pspnet()
+# model = pspnet(input_shape=(TRAIN_SIZE, TRAIN_SIZE, 3))
 
 # 对标签进行分类编码：变成one-hot编码
 # train_labels_final = keras.utils.to_categorical(ground_truth)

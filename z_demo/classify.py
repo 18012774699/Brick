0e29c6f5f405e1cab86dd6cd9e57128be2152801
Will <2623556566@qq.com>
2020-06-21 12:25:41 +0800

    asd

--- z_demo/classify.py
+++ z_demo/classify.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
-from cnn_net.res_block import ResNet
+import efficientnet.tfkeras as efn
 from cnn_net.resnet101 import get_resnet101
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -31,11 +31,11 @@ with tf.name_scope('tool'):
 
 
 with tf.name_scope('img_preprocess'):
-    label_path = r"E:\dataset\Kaggle\aptos2019-blindness-detection\train.csv"
+    label_path = r"D:\Friedrich\dataset\Kaggle\aptos2019-blindness-detection\train.csv"
     label_data = pd.read_csv(label_path)
     image_name = label_data["id_code"].values
     label = label_data["diagnosis"].values
-    image_path = r"E:\dataset\Kaggle\aptos2019-blindness-detection\train_images/" + image_name + ".png"
+    image_path = r"D:\Friedrich\dataset\Kaggle\aptos2019-blindness-detection\train_images/" + image_name + ".png"
 
     # 加载图片，过滤尺寸太小的图片
     for index in range(len(image_path)):
@@ -94,22 +94,25 @@ with tf.name_scope('data_preprocess'):
 
 with tf.name_scope('train'):
     batch_size = 32
-    lr = 1e-1
+    lr = 1e-3
     loss_func = 'sparse_categorical_crossentropy'  # 稀疏标签（0~20）
     num_classes = 5
 
-    # lr_schedule = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1)
+    lr_schedule = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1)
     early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
     save_model = keras.callbacks.ModelCheckpoint("my_keras_model.h5", save_best_only=True, save_weights_only=True)
 
     s = 20 * len(X_train) // batch_size
     learning_rate = keras.optimizers.schedules.ExponentialDecay(lr, s, 0.1)
-    optimizer = keras.optimizers.Adam(learning_rate, clipvalue=1.0)
     # optimizer = keras.optimizers.Adam(learning_rate)
+    optimizer = keras.optimizers.Adam(learning_rate, clipvalue=1.0)
+    # optimizer = keras.optimizers.Adam(lr, clipvalue=1.0)
 
-    image_input = keras.layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
-    C1, C2, C3, C4, C5 = get_resnet101(image_input, activation='relu')
-    x = C5
+    # image_input = keras.layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+    # C1, C2, C3, C4, C5 = get_resnet101(image_input, activation='relu')
+    # x = C5
+    base_model = efn.EfficientNetB3(input_shape=(IMG_SIZE, IMG_SIZE, 3), weights=None, include_top=False)
+    x = base_model.output
     x = keras.layers.MaxPooling2D(pool_size=2, strides=2)(x)
     x = keras.layers.Flatten()(x)
     x = keras.layers.Dense(64, activation="relu")(x)
@@ -120,7 +123,7 @@ with tf.name_scope('train'):
     x = keras.layers.Dropout(0.4)(x)
     output = keras.layers.Dense(num_classes, activation="softmax")(x)
 
-    model = keras.Model(inputs=[image_input], outputs=[output])
+    model = keras.Model(inputs=[base_model.input], outputs=[output])
     # model = keras.models.load_model("my_keras_model.h5")
     model.load_weights("my_keras_model.h5")
 
